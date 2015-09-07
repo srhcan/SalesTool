7f4128e60d346bc474ba3e808e13348c41ab5c70
SRHCAN <srhcan@gmail.com>
2015-09-07 16:11:07 -0400

    Initial checkin of sales tool.

---
+++ SalesTool/src/ca/demo/salestool/Main.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package ca.demo.salestool;
+
+/**
+ * @author Rizwan
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+		
+		displayGreeting();
+		data.display();
+
+	}
+	
+	private static void displayGreeting() {
+		System.out.println("HELLO HAPPY SALES PEOPLE!");
+		System.out.println("THIS APP SHOWS SALES DATA");
+	}
+
+}
---
+++ SalesTool/src/ca/demo/salestool/SalesData.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package ca.demo.salestool;
+
+/**
+ * @author Rizwan
+ *
+ */
+public class SalesData {
+	int data[] = {0, 4, 42};
+
+	public void display() {
+		System.out.println("TODO: Implement data display");
+	}
+}
